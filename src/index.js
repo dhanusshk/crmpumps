43bb31eb2419c8e88df06b05ead0971c58b0ab74
Dhanusshkumar <dhanussh07@gmail.com>
2019-10-16 16:18:17 +0530

    update 0.1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
-
+import logo from "./logo.png";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <img src={logo} alt="Logo" />;<h1>hello</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
+// Tell Webpack this JS file uses this image
+console.log(logo); // /logo.84287d09.png
+function Header() {
+  // Import result is the URL of your image
+  return <img src={logo} alt="Logo" class="center" />;
+}
+export default Header;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Header />, rootElement);
